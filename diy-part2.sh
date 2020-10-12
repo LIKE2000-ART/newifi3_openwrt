75cec45a3c351be730ccec6a43afaeca574c9d8e
LIKE2000-ART <55705638+LIKE2000-ART@users.noreply.github.com>
2020-10-12 15:31:32 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,11 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.1/g' package/base-files/files/bin/config_generate
+
+git clone https://github.com/chenhw2/luci-app-aliddns.git package/lean/luci-app-aliddns
+
+git clone --single-branch --branch master https://github.com/vernesong/OpenClash.git
+
+git clone https://github.com/tty228/luci-app-serverchan.git package/lean/luci-app-serverchan
+
