922a1e3110627a4b4323599007f8f16a418a4ab8
LIKE2000-ART <55705638+LIKE2000-ART@users.noreply.github.com>
2020-10-12 21:49:12 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,8 +15,6 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 
 git clone https://github.com/chenhw2/luci-app-aliddns.git package/lean/luci-app-aliddns
 
-git clone --single-branch --branch master https://github.com/vernesong/OpenClash.git
-
 git clone https://github.com/tty228/luci-app-serverchan.git package/lean/luci-app-serverchan
 
 rm -rf OpenClash
