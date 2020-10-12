e63e0231e05914d5d02d1fc01b49ac7d0dae1f3e
LIKE2000-ART <55705638+LIKE2000-ART@users.noreply.github.com>
2020-10-13 00:24:00 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,19 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+git clone https://github.com/chenhw2/luci-app-aliddns.git package/lean/luci-app-aliddns
+
+git clone https://github.com/tty228/luci-app-serverchan.git package/lean/luci-app-serverchan
+
+git clone --single-branch --branch master https://github.com/vernesong/OpenClash.git
+cd OpenClash
+mv luci-app-openclash ../package/lean/luci-app-openclash
+
+curl -sL -m 30 --retry 2 https://github.com/vernesong/OpenClash/releases/download/Clash/clash-"$CPU_MODEL".tar.gz -o /tmp/clash.tar.gz
+tar zxvf /tmp/clash.tar.gz -C /tmp >/dev/null 2>&1
+chmod +x /tmp/clash >/dev/null 2>&1
+mkdir -p .../package/luci-app-openclash/files/etc/openclash/core
+mv /tmp/clash .../package/luci-app-openclash/files/etc/openclash/core/clash >/dev/null 2>&1
+rm -rf /tmp/clash.tar.gz >/dev/null 2>&1
+
